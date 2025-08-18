72c3b04caa5a1cc8d4d8099aabf4b69d542d9aa6
Danylo Skepskyi <kol230305@gmail.com>
2025-08-18 21:31:14 +0200

    Dev: registered models in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from user.models import User
+
+admin.site.unregister(Group)
+admin.site.register(User)
