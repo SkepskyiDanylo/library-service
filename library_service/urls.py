854a0ee96f44f2beec000311557eb683a829d8aa
Danylo Skepskyi <kol230305@gmail.com>
2025-08-18 19:30:52 +0200

    Dev: changed uri

--- library_service/urls.py
+++ library_service/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/book/", include("book.urls", namespace="book")),
+    path("api/book-service/", include("book.urls", namespace="book")),
 ]
