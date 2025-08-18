09762609065dff6a0549f14db3d46a8115645c29
Danylo Skepskyi <kol230305@gmail.com>
2025-08-18 21:53:39 +0200

    Dev: added celery/celery-backend-results/celery-beat support

--- library_service/settings.py
+++ library_service/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "django_celery_results",
+    "django_celery_beat",
     "library_service",
     "payment",
     "borrowing",
@@ -131,3 +133,11 @@ REST_FRAMEWORK = {
         "refresh": "100/minute",
     },
 }
+
+CELERY_TIMEZONE = "UTC"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_RESULT_BACKEND = "django-db"
+
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
