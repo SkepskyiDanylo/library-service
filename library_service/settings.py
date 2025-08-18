556e8d6fb144bad02c8f46c2648bd8b97ff3cc87
Danylo Skepskyi <kol230305@gmail.com>
2025-08-18 19:17:53 +0200

    Dev: added django_restframework

--- library_service/settings.py
+++ library_service/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_restframework",
     "library_service",
     "payment",
     "borrowing",
