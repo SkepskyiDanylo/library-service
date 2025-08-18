c35f128e58c8a48ec5ad173fad8c4d94e756c2d9
Danylo Skepskyi <kol230305@gmail.com>
2025-08-18 19:25:35 +0200

    Dev: added BookViewSet

--- book/views.py
+++ book/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from book.models import Book
+from book.permissions import BookPermission
+from book.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (BookPermission,)
