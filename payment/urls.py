4726803f7b6782ca17dc6deb89dfc653d1728f39
Danylo Skepskyi <kol230305@gmail.com>
2025-08-19 20:03:05 +0200

    Dev: added success/cancel endpoints for stripe session

--- payment/urls.py
+++ payment/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from payment.views import PaymentViewSet
+from payment.views import PaymentViewSet, SuccessPaymentView, CancelPaymentView
 
 router = routers.DefaultRouter()
 router.register("", PaymentViewSet, basename="payment")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("session/success/", SuccessPaymentView.as_view(), name="success"),
+    path("session/cancel/", CancelPaymentView.as_view(), name="cancel"),
 ]
 
 app_name = "payment"
--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-
+from rest_framework import viewsets, status
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.utils.translation import gettext as _
 from payment.models import Payment
 from payment.serializers import PaymentDetailSerializer, PaymentListSerializer
+from payment.stripe_sessions import check_session_paid
 
 
 class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
@@ -21,3 +25,29 @@ class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
         if self.action == "retrieve":
             return PaymentDetailSerializer
         return PaymentListSerializer
+
+
+class SuccessPaymentView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        session = request.GET.get("session_id")
+        session_status = check_session_paid(session)
+        if session_status is True:
+            payment = get_object_or_404(Payment, session_id=session)
+            payment.status = "PAID"
+            payment.save()
+            return Response(
+                {"detail": _("Payment was successful.")}, status=status.HTTP_200_OK
+            )
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class CancelPaymentView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        return Response(
+            {"detail": _("Payment was unsuccessful. You can pay later.")},
+            status=status.HTTP_200_OK,
+        )
